6abeba8d95239e1dbbae2b9030e4877e06893627
quan41 <quannguyen888@gmail.com>
2016-09-08 10:43:22 -0700

    Added Files

---
+++ Lab_02.java
@@ -0,0 +1,26 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Exercise #1");
+		int a = 2;
+		int b = 4;
+		int product = a * b;
+		
+		System.out.println("2 multiplied by 4 is " + product);
+		
+		int c = 5;
+		int d = 6;
+		int product2 = c * d;
+		
+		System.out.println("5 multiplied by 6 is " + product2);
+		
+		System.out.println("Exercise #2");
+		
+		int e =
+		
+		
+	}
+	
+	
+}
---
+++ firstProg.java
@@ -0,0 +1,14 @@
+public class firstProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World");
+		System.out.println("Hello Again");
+		System.out.println(" \"\" ");
+		System.out.println("^   ");
+		System.out.println("^^  ");
+		System.out.println("^^^ ");
+		System.out.println("^^^^");
+	}
+
+}
