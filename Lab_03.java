e4c0fdb9d9cf3b6f5b5c53fa5dbd15d03714932e
quan41 <quannguyen888@gmail.com>
2016-09-12 11:12:54 -0700

    Added Files

---
+++ Lab_03.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; //import Statement]
+public class Lab_03
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Whats your name?");
+		String name = keyboard.next();
+		System.out.println("Wow... " + name + " is such a dumb name...");
+		System.out.println("How old are you?");
+		int num = keyboard.nextInt();
+		System.out.println("Wow how many times did you get held back?");
+		System.out.println("What do you do for fun?");
+		String hobby = keyboard.next();
+		System.out.println("Honestly thats pretty lame.");
+		System.out.println("What music do you listen to?");
+		String music = keyboard.next();
+		System.out.println("You have really bad taste.");
+		System.out.println("How many siblings do you have?");
+		String siblings = keyboard.next();
+		System.out.println("I feel bad for your mother.");
+		System.out.println("What do you wanna be when you grow up?");
+		String future = keyboard.next();
+		System.out.println("Yeah...thats not gonna happen.");
+		System.out.println("Okay stop talking to me now.");
+		
+		
+		
+	}	
+	
+	
+	
+	
+}
