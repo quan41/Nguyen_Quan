0ec423f2e71f254ac9e037a3a68cdda1eb3b6875
quan41 <quannguyen888@gmail.com>
2016-09-30 11:58:47 -0700

    lesson 4

---
+++ AP LESSON 04/Rectangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Rectangle
+{
+	public static void main(String[]args)
+	{
+		Rectangle receipt = new Rectangle();
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("Please enter the length of the rectangle: ");
+		double l = kb.nextDouble();
+		
+		
+		System.out.println("Please enter the width of the rectangle: ");
+		String dummy = kb.nextLine();
+		double w = kb.nextDouble();
+		double p = 2 * w + 2 * l;
+		System.out.println("Your rectangle is " + p + " feet around");
+		
+	
+	} 
+
+	public void format(double p)
+	{
+		System.out.printf("\n%10s.....%99999999999.5f", p);
+	}
+}
