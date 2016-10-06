e478e0edd969896937ff5306bc0f58394c4d1a9f
quan41 <quannguyen888@gmail.com>
2016-10-06 11:47:06 -0700

    Lab 1 + 2

---
+++ AP LESSON 02/Lab_02.java
@@ -0,0 +1,39 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Exercise #1");
+		int a = 2;
+		int b = 4;
+		int product = a * b;
+		
+		System.out.println("2 multiplied by 4 is " + product);
+		
+		int c = 5;
+		int d = 6;
+		int product2 = c * d;
+		
+		System.out.println("5 multiplied by 6 is " + product2);
+		
+		System.out.println("Exercise #2");
+		
+		String name = "Quan Nguyen";
+		String address = "Apple Street";
+		String City = "San Diego";
+		String State = "CA";
+		int zip = 92130;
+		
+		System.out.println(name);
+		System.out.println(address);
+		System.out.println(City + " , " + State + " , " + zip);
+		
+		System.out.println("Exercise #3");
+		
+		int h = 2;
+		int l = 2;
+		int w = 2;
+		System.out.println("The surface area of your shape is " + ((2 * l * h) + (2 * w * h)));
+	}
+	
+	
+}
